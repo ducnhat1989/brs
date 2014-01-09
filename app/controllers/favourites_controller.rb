ac1a86c596ddc7e6a2750c0c301a7dd64a20b128
Nhat Truong Duc <truong.duc.nhat@framgia.com>
2014-01-09 09:28:49 +0700

    Show favourite book list

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -1,9 +1,14 @@
 class FavouritesController < ApplicationController
 
+  def index
+    @favourites = Favourite.where(user_id: params[:user_id]).
+                   paginate page: params[:page], per_page: 3
+  end
+
   def create
     activity_favourite = Activity.create! user_id: current_user.id
-    Favourite.create! user_id: params[:favourite][:user_id], 
-                 book_id: params[:favourite][:book_id],
+    Favourite.create! user_id: current_user.id, 
+                 book_id: params[:book_id],
                  activity_id: activity_favourite.id
   end
 end
